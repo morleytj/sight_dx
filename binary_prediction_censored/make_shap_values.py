038246fb2e74ccbfe8047b8eee7f49c0ac421e74
Theodore Morley <ted@elmgatefarm.com>
2023-05-31 15:39:49 -0500

    first draft before running shapvals after finishing both functions

--- binary_prediction_censored/make_shap_values.py
+++ binary_prediction_censored/make_shap_values.py
@@ -41,28 +41,43 @@ def runshap(df, cols, clf, background, out):
 '''
 Input:
     df: contains actual data for some number of individuals, at least including the person_id/dID combo of interest
-    person_id: Person_id of the person we want plotted
+    sdf: contains shap values
+    pid: Person_id of the person we want plotted
     did: dID we want plotted for the person we want plotted
     expected_val: expected value from the explainer
     cols: columns in order for the classifier
-    fnames: feature names in clf order to be displayed on plot
     out: path to save results to
+    contr: contribution threshold as in shap force plots, set default to 0.05
 '''
-def gen_force_plot(df, person_id, did, expected_val, cols, fnames, out):
-    #
-    shap.plots.force(expected_val, shap_values=row, features=df[cols], matplotlib=True,show=False, feature_names=fnames)
+def gen_force_plot(df, pid, did, expected_val, cols, out, contr=0.05):
+    cols = [c for c in cols if c not in ['cc_status','dID','case_prob','PERSON_ID']]
+    feats = df.loc[(df.person_id==int(pid))&(df.dID==int(did)),cols].values
+    row = sdf.loc[(df.person_id==int(pid))&(df.dID==int(did)),cols].values
+    shap.plots.force(expected_val, shap_values=row, features=feats, matplotlib=True,show=False, feature_names=cols, contribution_threshold=contr)
     plt.savefig(out+str(person_id)+'_'++str(did)+'_force_plot.png')
 
 
-#python scriptname genshap --
+#python scriptname genshap cols df clf out bg
 #or
-#python scriptname forceplot --
+#python scriptname forceplot cols df clf out personid did expval fnames (optional)contr
 #load in and format files, run the runshap function
 if __name__=="__main__":
-    if sys.argv[1]='genshap':
-        #
-    elif sys.arg[1]='forceplot':
-        #
+    fc = open(sys.argv[2], 'rb')
+    col = pickle.load(fc)
+    fc.close()
+    df = pd.read_csv(sys.argv[3])
+    clf = joblib.load(sys.argv[4])['classify']
+    out = sys.argv[5]
+    if sys.argv[1]=='genshap':
+        bg = pd.read_csv(sys.argv[6])
+        sv, exp = runshap(df, col, clf, bg, out)
+    elif sys.arg[1]=='forceplot':
+        personid=sys.argv[6]
+        did=sys.argv[7]
+        expval=int(sys.argv[8])
+        if len(sys.argv)>9:
+            contr = float(sys.argv[9])
+            gen_force_plot(df, personid, did, expval, col, fnames, out, contr)
+        gen_force_plot(df, personid, did, expval, col, out)
     else:
         print("Input command not recognized. Please use either 'genshap' or 'forceplot'.")
-    #fc = open(colsloc, 'rb')
