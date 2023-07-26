23b591e3bf124858c19ee7fe9004fe9a72cca2cd
Theodore Morley <ted@elmgatefarm.com>
2023-07-26 12:00:42 -0500

    small script to batch out person_id files

---
+++ make_person_id_batches.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import sys
+
+if __name__=="__main__":
+    main_df = pd.read_csv(sys.argv[1])
+    out_path = sys.argv[2]
+    unique_pid = main_df['PERSON_ID'].unique()
+    for p in unique_pid:
+        main_df.loc[main_df.PERSON_ID==p].to_csv(out+str(p)+'_all_did.csv',index=False)
