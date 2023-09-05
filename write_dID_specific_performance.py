8e284ec534a983232b9770ff87be0db043edba89
Theodore Morley <ted@elmgatefarm.com>
2023-09-05 15:22:19 -0500

    helper script for writing stats

---
+++ write_dID_specific_performance.py
@@ -0,0 +1,5 @@
+f=open('performance_by_dID_cleaned.csv','w')for did in cleaned.dID.unique():
+for did in cleaned.dID.unique():
+    if len(cleaned.loc[cleaned.dID==did,'cc_status'].unique())>1:
+        f.write(str(did)+','+str(roc_auc_score(cleaned.loc[cleaned.dID==did,'cc_status'], cleaned.loc[cleaned.dID==did,'censored_cases_uncen_controls_prob']))+'\n')
+f.close()
